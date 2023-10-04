87d2b5ebfba46a9477ccd577c09d03295e874d72
Lárraga Maldonado <glarragw@jyu.fi>
2023-10-04 14:58:12 +0300

    Added header and menu as components

---
+++ header.php
@@ -0,0 +1,47 @@
+<?php
+  echo '<div class="corporate-header-top">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12 col-lg-8 col-xl-8 col-md-12 col-sm-12">
+          <div class="top-address text-left">
+            <p>
+              <span>
+                <i class="icofont-external-link"></i
+                ><a href="https://desdeo.it.jyu.fi/"
+                  >Discover DESDEO: Enhancing Decision-Making with Our
+                  Software Framework.</a
+                ></span
+              >
+              <!--                   <a href="mailto:demo@example.com">
+                <i class="icofont-envelope"></i>
+              </a> -->
+            </p>
+          </div>
+        </div>
+        <!-- TOP RIGHT -->
+        <div class="col-xs-12 col-lg-4 col-xl-4 col-md-12 col-sm-12">
+          <div class="top-right-menu">
+            <ul class="social-icons text-right text_m_center">
+              <li>
+                <a class="github social-icon" href="#"
+                  ><i class="icofont-github"></i
+                ></a>
+              </li>
+              <li>
+                <a class="twitter social-icon" href="#"
+                  ><i class="icofont-twitter"></i
+                ></a>
+              </li>
+              <li>
+                <a class="youtube social-icon" href="#"
+                  ><i class="icofont-brand-youtube"></i
+                ></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>'
+?>
+     
---
+++ menu.php
@@ -0,0 +1,80 @@
+
+<?php
+  echo '<nav class="corporate_menu">
+    <ul class="sub-menu">
+      <li>
+        <a href="index.php">Home</a>
+      </li>
+      <li class="menu-item-has-children">
+        <a href="#">About us</a>
+        <ul class="sub-menu">
+          <li>
+            <a href="our_group_in_brief.php"
+              >Our group in brief</a
+            >
+          </li>
+          <li>
+            <a href="research_interests.php"
+              >Our research interests</a
+            >
+          </li>
+          <li><a href="gallery.php">Photo gallery</a></li>
+        </ul>
+      </li>
+      <li class="menu-item-has-children">
+        <a href="#">Members</a>
+        <ul class="sub-menu">
+          <li>
+            <a href="members.php">Current members</a>
+          </li>
+          <li>
+            <a href="former_members.php">Former members</a>
+          </li>
+          <li>
+            <a href="long_term_visitors.php"
+              >Long term visitors</a
+            >
+          </li>
+          <li>
+            <a href="join_our_group.php">Join our group</a>
+          </li>
+        </ul>
+      </li>
+      <li class="menu-item-has-children">
+        <a href="#">Research activities</a>
+        <ul class="sub-menu">
+          <li><a href="seminar.php">Weekly seminar</a></li>
+          <li><a href="projects.php">Projects</a></li>
+          <li>
+            <a href="conference_participation.php"
+              >Conference participation</a
+            >
+          </li>
+          <li>
+            <a href="research_visits.php">Research visits</a>
+          </li>
+          <li>
+            <a href="posters_of_research.php"
+              >Posters of research</a
+            >
+          </li>
+          <li>
+            <a href="software.php">Software</a>
+          </li>
+        </ul>
+      </li>
+      <li class="menu-item-has-children">
+        <a href="#">Publications</a>
+        <ul class="sub-menu">
+          <li>
+            <a href="publications.php">List of publications</a>
+          </li>
+          <li><a href="material.php">Extra material</a></li>
+        </ul>
+      </li>
+      <li><a href="news.php">News</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </nav>'
+              
+?>
