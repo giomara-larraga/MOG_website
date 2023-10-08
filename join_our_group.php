f68bf6e301df35d10879a4e4acde5bbed59dd660
giomara-larraga <giomara.lm@gmail.com>
2023-10-08 21:42:19 +0300

    Added content to projects, material and join our group

---
+++ join_our_group.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Multiobjective Optimization Group</title>
+    <meta name="description" content="" />
+    <meta
+      name="viewport"
+      content="wicorporateh=device-wicorporateh, initial-scale=1"
+    />
+    <!-- Favicon Icon -->
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="venobox/venobox.css" />
+    <link rel="stylesheet" type="text/css" href="css/plugin_theme_css.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/corporate_style.css" />
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="css/corporate-responsive.css"
+    />
+    <!-- modernizr js -->
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+  </head>
+  <body>
+    <div class="em40_header_area_main">
+    <?php include 'header.php'?>
+      <!-- END HEADER TOP AREA -->
+      <div class="corporate-main-menu one_page hidden-xs hidden-sm witr_h_h10">
+        <div class="corporate_nav_area scroll_fixed postfix">
+          <div class="container-fluid">
+            <div class="row logo-left">
+              <div class="col-md-3 col-sm-3 col-xs-4">
+                <div class="logo">
+                  <a class="main_sticky_main_l" href="index.php" title="MOG">
+                    <img src="img/logo1.png" alt="MOG" />
+                  </a>
+                  <a class="main_sticky_l" href="index.php" title="MOG">
+                    <img src="img/logo2.png" alt="MOG" />
+                  </a>
+                </div>
+              </div>
+              <!-- MAIN MENU -->
+              <div class="col-md-9 col-sm-9 col-xs-8">
+                <div class="tx_mmenu_together">
+                <?php include 'menu.php'?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- MOBILE MENU Logo AREA -->
+    <div class="mobile_logo_area hidden-md hidden-lg">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="mobile_menu_logo text-center">
+              <a href="index.php" title="poket">
+                <img src="img/logo1.png" alt="image" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- MOBILE MENU AREA -->
+    <div class="home-2 mbm hidden-md hidden-lg header_area main-menu-area">
+      <div class="menu_area mobile-menu">
+      <?php include 'menu.php'?>
+      </div>
+    </div>
+    <!-- END MOBILE MENU AREA  -->
+    <!-- breadcumb area -->
+    <div class="breadcumb-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 txtc text-center ccase">
+            <div class="brpt">
+              <h2>Join our group</h2>
+            </div>
+            <div class="breadcumb-inner">
+              <ul>
+                <li>
+                  You are currently here! <i class="fas fa-angle-right"></i>
+                </li>
+                <li>
+                  <a href="index.php" rel="v:url" property="v:title">Home</a>
+                </li>
+                <li><i class="fas fa-angle-right"></i></li>
+                <li><span class="current">Join our group</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End breadcumb area -->
+    <!-- single service area -->
+    <div class="single_service_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="witr_text_widget">
+              <div class="witr_text_widget_inner">
+                <img src="img/gallery/UI-test.JPG" alt="image" />
+                <h2>Join the Multiobjective Optimization Group</h2>
+                
+                <p>We welcome both new doctoral studends and postdoctoral researchers assuming
+funding can be arranged. Information about these positions and the requirements in English 
+is provided <a
+href="https://www.jyu.fi/it/en/open-positions/mit">here</a>. Motivated bachelor and master 
+students are also welcomed to conduct their interested research in the industrial optimization group. <a
+href="http://www.mit.jyu.fi/optgroup/thesis_topics.html"> <b> There are several thesis topics available for bachelor, master, and Ph.D. students. </b></a> </p>
+
+<h3>Requirements for new postdoctoral researchers</h3>
+<ul class="default-list">
+<li>Be familiar with multiobjective optimization and basics of nonlinear
+programming</li>
+<li>Have high interest and plan of how to collaborate with the Industrial
+Optimization Group and prepare joint publications
+<li> Be interested in and willing to work as an active part of the group
+<li> Be active in research and willing to bear responsibility of one's own
+duties within the group
+<li>Be willing to supervise doctoral students and share one's knowledge and
+expertise with group members
+<li>Have experience and interest in teaching optimization courses
+<li> Have experience in coordinating research projects or interest, experience
+and contacts in finding new research projects
+<li>Have experience in scientific publishing and reviewing research articles
+</ul>
+<br>
+<h3>Requirements for new doctoral students</h3>
+<ul class="default-list">
+<li> Be familiar with basics of multiobjective optimization and nonlinear
+programming
+<li>Be motivated in working hard in theory/method/software development of
+nonlinear multiobjective optimization and industrial applications
+<li>Have motivation and preliminary plan of how to collaborate with the
+Industrial Optimization Group
+<li>Have interest in preparing scientific research articles 
+<li>Be willing to work as an active member of the group and bear
+responsibility of one's own duties within the group
+<li>General requirements</b> for doctoral students are listed <a 
+href="https://www.jyu.fi/it/en/study/postgraduate/application/">here</a>.</li>
+</ul>
+
+<br>
+
+
+<p>E-mails with CV and recommendation letters should be sent to Prof. <a href="http://users.jyu.fi/~miettine/index.html">Kaisa Miettinen</a> <a 
+href="MAILTO:kaisa.miettinen@jyu.fi">kaisa.miettinen at 
+jyu.fi</a>.</p>    
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+    <!-- footer area -->
+
+      <?php include 'footer.php'?>
+    <!-- Include All JS -->
+    <script src="js/vendor/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.nivo.slider.pack.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="venobox/venobox.min.js"></script>
+    <script src="js/jquery.appear.js"></script>
+    <script src="js/jquery.knob.js"></script>
+    <script src="js/BeerSlider.js"></script>
+    <script src="js/theme-pluginjs.js"></script>
+    <script src="js/jquery.meanmenu.js"></script>
+    <script src="js/ajax-mail.js"></script>
+    <script src="js/theme.js"></script>
+  </body>
+</html>
---
+++ material.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Multiobjective Optimization Group</title>
+    <meta name="description" content="" />
+    <meta
+      name="viewport"
+      content="wicorporateh=device-wicorporateh, initial-scale=1"
+    />
+    <!-- Favicon Icon -->
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="venobox/venobox.css" />
+    <link rel="stylesheet" type="text/css" href="css/plugin_theme_css.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/corporate_style.css" />
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="css/corporate-responsive.css"
+    />
+    <!-- modernizr js -->
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+  </head>
+  <body>
+    <div class="em40_header_area_main">
+    <?php include 'header.php'?>
+      <!-- END HEADER TOP AREA -->
+      <div class="corporate-main-menu one_page hidden-xs hidden-sm witr_h_h10">
+        <div class="corporate_nav_area scroll_fixed postfix">
+          <div class="container-fluid">
+            <div class="row logo-left">
+              <div class="col-md-3 col-sm-3 col-xs-4">
+                <div class="logo">
+                  <a class="main_sticky_main_l" href="index.php" title="MOG">
+                    <img src="img/logo1.png" alt="MOG" />
+                  </a>
+                  <a class="main_sticky_l" href="index.php" title="MOG">
+                    <img src="img/logo2.png" alt="MOG" />
+                  </a>
+                </div>
+              </div>
+              <!-- MAIN MENU -->
+              <div class="col-md-9 col-sm-9 col-xs-8">
+                <div class="tx_mmenu_together">
+                <?php include 'menu.php'?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- MOBILE MENU Logo AREA -->
+    <div class="mobile_logo_area hidden-md hidden-lg">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="mobile_menu_logo text-center">
+              <a href="index.php" title="poket">
+                <img src="img/logo1.png" alt="image" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- MOBILE MENU AREA -->
+    <div class="home-2 mbm hidden-md hidden-lg header_area main-menu-area">
+      <div class="menu_area mobile-menu">
+      <?php include 'menu.php'?>
+      </div>
+    </div>
+    <!-- END MOBILE MENU AREA  -->
+    <!-- breadcumb area -->
+    <div class="breadcumb-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 txtc text-center ccase">
+            <div class="brpt">
+              <h2>Extra material</h2>
+            </div>
+            <div class="breadcumb-inner">
+              <ul>
+                <li>
+                  You are currently here! <i class="fas fa-angle-right"></i>
+                </li>
+                <li>
+                  <a href="index.php" rel="v:url" property="v:title">Home</a>
+                </li>
+                <li><i class="fas fa-angle-right"></i></li>
+                <li><span class="current">Extra material</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End breadcumb area -->
+    <!-- single service area -->
+    <div class="single_service_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="witr_text_widget">
+              <div class="witr_text_widget_inner">
+                <h2>Supplementary material</h2>
+                
+                <ul class="default-list">
+                <li><b>Afsar, B., Silvennoinen, J., Ruiz,  F., Ruiz, A.B., Misitano, G.,
+  Miettinen, K.</b>, <i>An experimental design for comparing
+  interactive methods based on  their desirable properties</i>, manuscript.
+<a href="https://nextcloud.jyu.fi/index.php/s/QtNAkQNFrijn6Hq">Supplementary
+  material in NextCloud</a> (Sustainability problem in brief, overviews
+  of interactive methods compared, user interface designs for them,
+  material used in the experiments in Spanish) 
+
+
+<li>
+<b><a name="2021-1"></a>Afsar, B., Silvennoinen, J., Misitano, G., Ruiz,  F., Ruiz, A.B.,
+  Miettinen, K.</b>, <i>Designing Empirical Experiments to Compare
+Interactive Multiobjective Optimization Methods</i>, to appear in the
+  Journal of Operational Research Society.
+<a href="https://nextcloud.jyu.fi/index.php/s/jRyZpweEjWWfXaB">Supplementary
+  material in NextCloud</a> (Sustainability problem and interactive
+  methods compared); 
+<a href="https://nextcloud.jyu.fi/index.php/s/pEeAAHdPNYcjwTc">Supplementary
+  material in NextCloud</a> (Summary of the sustainability problem)
+
+</li>
+
+<li>
+<b>Afsar B., Miettinen, K., Ruiz, A.B.</b>, <i>Artificial Decision
+  Maker for Comparing Reference Point Based Interactive Evolutionary
+  Multiobjective Optimization Methods</i>, in &quot;11th International
+Conference Series on Evolutionary Multi-Criterion Optimization (EMO
+2021), Proceedings&quot;, Edited by H. Ishibuchi, Q. Zhang, R. Cheng,
+  K. Li, H. Li, H. Wang, A. Zhou, Springer, 619-631, 2021.  <a href="https://github.com/industrial-optimization-group/adm-emo/blob/emo2021/EMO_2021__supplementary_document.pdf">Supplementary material on github</a>.
+<a href="https://github.com/industrial-optimization-group/adm-emo"> Code </a>
+</li>
+
+<li>
+<B>Lovison, A., Miettinen, K.</B>, <I>On the Extension of the DIRECT
+ Algorithm to Multiple Objectives</I>, Journal of Global Optimization,
+ 79(2), 387-412, 2021.
+<A href="https://www.geogebra.org/m/fk6nh8ws">Supplementary material in GeoGebra</A>
+</li>
+
+
+<li>
+<b>Mazumdar, A., Chugh, T., Hakanen, J., Miettinen, K.</b>, <i>An
+  Interactive Framework for Offline Data-Driven Multiobjective
+  Optimization</i>, in &quot;9th International Conference on
+Bioinspired Optimisation Methods and Their Applications (BIOMA 2020),
+  Proceedigs&quot;, Edited by B Filipic, E. Minisci, M. Vasile,
+  Springer, 97-209, 2020. <a href="https://github.com/industrial-optimization-group/BIOMA_2020_Supplementary_Mazumdar">Supplementary material on github</a>,
+</li>
+
+
+
+<li>
+<b>Habib, A., Singh, H.K., Chugh, T., Ray, T. Miettinen, K.</b>, <i>A Multiple Surrogate Assisted Decomposition-Based Evolutionary Algorithm for Expensive Multi/Many-Objective Optimization</i>, IEEE Transactions on Evolutionary Computation, 23(6), 1000-1014, 2019. <a href="http://www.mdolab.net/research_resources.html">Supplementary material</a>
+</li>
+
+<li>
+<b>Mazumdar, A., Chugh, T.,  Miettinen, K., Lopez-Ibanez, M.</b> <i>On
+  Dealing with Uncertainties from Kriging Models in Offline
+  Data-driven Evolutionary Multiobjective Optimization</i>, in
+&quot;10th International Conference on Evolutionary Multi-Criterion
+Optimization (EMO 2019), Proceedings&quot;, Edited by K. Deb,
+  E. Goodman, C. A. C. Coello, K. Klamroth, K. Miettinen,
+  S. Mostaghim, P. Reed, Springer, 463-474, 2019. <a href="https://github.com/industrial-optimization-group/EMO2019_Supplementary_Mazumdar">Supplementary material on github</a>
+</li>
+
+
+<li>
+<B>Fieldsend, J.E., Chugh, T.,  Allmendinger, R.,  Miettinen, K.</B>, <I>A Feature Rich Distance-Based Many-Objective Visualisable Test Problem Generator, in "Proceedings of the Genetic and Evolutionary Computation Conference</I>,
+(GECCO-2019), Edited by M. Lopez-Ibanez, The Association of Computing
+Machinery (ACM), New York, 541-549, 2019.
+<A href="https://github.com/fieldsend/DBMOPP_generator"> Generator code in github</A>
+
+</li>
+
+<li>
+<B>Chugh, T., Allmendinger, R., Ojalehto, V., Miettinen, K.</B>, <I>A surrogate-assisted EMOA for problems with non-uniform objective latencies</I>,
+in &quot; Proceedings of the 23rd Genetic and Evolutionary Computation
+Conference (GECCO-2018)&quot;, Edited by H. Aguirre, The Association of
+Computing Machinery (ACM), 609-616, 2018.
+<A href="https://github.com/industrial-optimization-group/gecco2018-supplementary">Supplementary material on github</A>
+
+</li>
+</ul>
+
+                             
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+    <!-- footer area -->
+
+      <?php include 'footer.php'?>
+    <!-- Include All JS -->
+    <script src="js/vendor/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.nivo.slider.pack.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="venobox/venobox.min.js"></script>
+    <script src="js/jquery.appear.js"></script>
+    <script src="js/jquery.knob.js"></script>
+    <script src="js/BeerSlider.js"></script>
+    <script src="js/theme-pluginjs.js"></script>
+    <script src="js/jquery.meanmenu.js"></script>
+    <script src="js/ajax-mail.js"></script>
+    <script src="js/theme.js"></script>
+  </body>
+</html>
